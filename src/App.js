f48ac6a68dbbf2b78d6c7208d0f45e9d865fa85f
Zachary W Sarette <zacharywsarette@gmail.com>
2023-02-08 22:43:01 +0900

    Finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import {useState} from 'react';
-import {useEffect} from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg'
+const API_URL = 'http://omdbapi.com?apikey=34739720'
+const movie1 = {
+    "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+}
+const App = () => {
+    const [movies, setMovies] = useState([movie1])
+    const [searchTerm, setSearchTerm] = useState('')
 
-const App =()=> {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You've set the counter to " + counter)
-  }, [counter])
-  return (
-    <div className="App">
-      <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <h1>yes</h1>
-      <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
-    </div>
-  );
+    const searchMovies = async (title) => {
+         const response = await fetch(`${API_URL}&s=${title}`);
+         const data = await response.json()
+         console.log(data)
+         setMovies(data.Search)
+    } 
+    useEffect(()=>{
+        searchMovies('Spiderman')
+    }, [])
+    return(
+        <div className='app'>
+        <h1>MovieLand</h1>
+        <div class='search'>
+        <input
+            placeholder='Search for Movies'
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+        />
+        <img
+            src={SearchIcon}
+            alt='search'
+            onClick={() => searchMovies(searchTerm)}
+        />
+        </div>
+            <div className="container">
+            { 
+                movies?.length > 0 ?
+                movies?.map(
+                    e => <MovieCard key={e.Title + e.Year } {...e}/>
+                ) : (
+                <div className="empty">
+                    No movies found
+                </div>)
+            }
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+const MovieCard = ({Year, Poster, Title, Type}) => 
+           <div className='movie'>
+                <div>
+                    <p>{Year}</p>
+                </div>
+                <div>
+                    <img
+                    src={Poster !== "N/A" ? Poster : 'https://via.placeholder.com/400'}
+                    alt={Title}
+                    />
+                </div>
+                <div>
+                    <span>{Type}</span>
+                    <h3>{Title}</h3>
+                </div>
+            </div>
+
+export default MovieCard
