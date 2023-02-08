93e35aab6c82b3981365b929569f96593b40d2f8
Zachary W Sarette <zacharywsarette@gmail.com>
2023-02-08 21:24:33 +0900

    Counter with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import {useState} from 'react';
+import {useEffect} from 'react';
 import './App.css';
 
 const App =()=> {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You've set the counter to " + counter)
+  }, [counter])
   return (
     <div className="App">
       <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
