d08f0801023d6caea988c46ee10135fca33cf19a
Zachary W Sarette <zacharywsarette@gmail.com>
2023-02-08 21:09:06 +0900

    Person Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = ({name, lastName, age}) => 
+  <>
+  <h1>Name: {name}</h1>
+  <h1>Last Name: {lastName}</h1>
+  <h1>Age: {age}</h1>
+  </>
+
+
+const App =()=> {
+  const name = null; 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {2 * 2}!</h1>
+      {
+        name ? (
+          <>
+          test
+          </>
+        ):(
+          <>
+          <h1>test</h1>
+          <h1>There is no name</h1>
+          <Person name={'John'} lastName={'Doe'} age={34}/>
+          <Person name={'Jane'} lastName={'Doe'} age={24}/>
+          <Person name={'Santa'} lastName={'Claus'} age={533}/>
+          <Person name={'Willem'} lastName={'Mayhem'} age={11}/>
+          </>
+        )
+      }
     </div>
   );
 }
